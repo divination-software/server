5afeaabc053780c947d4159e1b12f80a1035daa0
lopez567 <spencer.r.lopez@gmail.com>
2016-11-16 10:24:20 -0800

    Add loading spinner

---
+++ public/source/components/Loading.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Loading = () => (<i className="material-icons loading">refresh</i>);
+export default Loading;
--- public/source/containers/Board.js
+++ public/source/containers/Board.js
@@ -1,4 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Loading from '../components/Loading';
 
-const Board = () => (<iframe className='board' src="/www/index.html"></iframe>);
-export default Board;
+export default class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    }
+  }
+  handleLoad() {
+    console.log('supppp');
+    this.setState({loading: false});
+  }
+  render() {
+    if (this.state.loading === true) {
+      return (<div>
+        <Loading />
+        <iframe style={{display: 'none'}} className='board' onLoad={e => this.handleLoad()} src="/www/index.html"></iframe>
+        </div>
+      )
+    }
+    return (<iframe className='board' onLoad={e => this.handleLoad()} src="/www/index.html"></iframe>)
+  }
+}
