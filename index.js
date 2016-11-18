c493f1155d7cf69ed9f63a68238c92499e034f69
Vorifin Layanto <vlayanto.at.dev@gmail.com>
2016-11-17 16:54:57 -0800

    Merge the package.json file

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bluebird = require('bluebird');
-mongoose.Promise = bluebird;
+const express = require('express');
+const mongoose = require('mongoose');
+const bluebird = require('bluebird');
+const socketHandler = require('./socketHandler');
 
-var app = express();
+// App init
+const app = express();
 
-var port = process.env.PORT || 3000;
-
-// configure our server with all the middleware and routing
+// Configure server with all the middleware and routing
 require('./middleware.js')(app, express);
 require('./routes.js')(app, express);
 
-app.listen(port, function() {
-  console.log('Listening on port ' + port);
+// Export App for testing
+module.exports = app;
+
+// Connect socket
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+mongoose.Promise = bluebird;
+
+// Connect to mongoDB unless testing
+const port = process.env.PORT || 3000;
+if (process.env.TEST === 'false') {
+  mongoose.connect(process.env.MONGODB_URL)
+}
+
+io.on('connection', socketHandler);
+
+// On connection , listen to port
+mongoose.connection.on('connected', () => {
+  server.listen(port, function() {
+    console.log('Listening on port ' + port);
+  })
 })
